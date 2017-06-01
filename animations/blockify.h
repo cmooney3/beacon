39df9eb7c05600970dd357f9c3833e24358d9f88
Charlie Mooney <cmooney3@gmail.com>
2017-05-31 19:00:38 -0700

    Add ball coloring to the Blockify animation
    Signed-off-by: Charlie Mooney <cmooney3@gmail.com>

--- animations/blockify.h
+++ animations/blockify.h
@@ -15,12 +15,17 @@ void BlockifyAnimation(CRGB *leds, int num_pole_leds, int num_ball_leds) {
   uint8_t num_blocks = kMinBlocks + rand() % (kMaxBlocks - kMinBlocks);
   int block_size = num_pole_leds / num_blocks;
   
-  CRGB c1, c2;
-  fillRandomContrastingColors(c1, c2);
+  CRGB pole_c1, pole_c2;
+  fillRandomContrastingColors(pole_c1, pole_c2);
+  CRGB ball_c1, ball_c2;
+  fillRandomContrastingColors(ball_c1, ball_c2);
   
   for (uint16_t i = 0; i < kNumFlashes; i++) {
     for (uint16_t j = 0; j < num_pole_leds; j++) {
-      leds[j] = (int)(j / block_size + i) % 2 ? c1 : c2;
+      leds[j] = (int)(j / block_size + i) % 2 ? pole_c1 : pole_c2;
+    }
+    for (uint16_t j = 0; j < num_ball_leds; j++) {
+      leds[num_pole_leds + 1 + j] = (i % 2) ? ball_c1 : ball_c2;
     }
 
     FastLED.show();
